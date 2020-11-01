472704ba5c6feef29f4987df1c616b99a3fd91f4
HonourableUSB <matthew.haskell229@gmail.com>
2020-10-31 22:39:31 -0500

    Finished up logic for event listening

--- EncryptionGUI/src/GUI.java
+++ EncryptionGUI/src/GUI.java
@@ -11,6 +11,10 @@ import java.io.File;
 
 public class GUI extends JFrame implements WindowListener,ActionListener{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final Character[] emptySpaceOrZeroEq = {'-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	private final Character[] aEqualsChoice = {'-','A','B','C','D','E','F','G','H','I','J',
 			'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -35,6 +39,11 @@ public class GUI extends JFrame implements WindowListener,ActionListener{
 	private JTextField outField = new JTextField("", 30);
 	File encryptFile;
 	File decryptFile;
+	private boolean isEncryption;
+	private boolean isFile;
+	private boolean allFieldsFilled;
+	private static boolean ready = false;
+	private static char[] cipherChoices;
 	
 	GUI()
 	{
@@ -74,6 +83,13 @@ public class GUI extends JFrame implements WindowListener,ActionListener{
 		setVisible(true);
 	}
 	
+	public boolean isAFile() { return isFile;}
+	
+	public boolean isEncryption() { return isEncryption;}
+	
+	public static boolean isReady() { return ready;}
+	
+	public static char[] getCipherChoices() {return cipherChoices;}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -104,6 +120,7 @@ public class GUI extends JFrame implements WindowListener,ActionListener{
 			//TODO What happens when you press the magic button?
 			/*  0. Check if the input field is a plaintext message
 			 *  1. Checks if file exists, if not, throw an error
+			 *  1.1 If encrypt has been pressed, change all values
 			 *  2. Check that all the dropdown menus are completed, if not throw an error
 			 *  3. ???
 			 *  4. Profit
@@ -111,20 +128,42 @@ public class GUI extends JFrame implements WindowListener,ActionListener{
 			//If the user manually entered the file, we don't want them getting avoided because they didn't use the file selection button!
 			encryptFile = new File(inField.getText());
 			String inputText = inField.getText();
+			isEncryption = true;
 			//if the input entered was plaintext (denoted by ** at the beginning)
 			if(inputText.startsWith("**"))
 			{
-				System.out.println("Text");
+				//System.out.println("Text");
+				isFile = false;
 			}
 			else if(encryptFile.exists()) //check file exists, if not throw an error
 			{
-				System.out.println("File");
+				//System.out.println("File");
+				isFile = true;
 			}
 			else
 			{
-				System.out.println("Not file nor text");
-				new JOptionPane("Please enter a file or message");
+				//System.out.println("Not file nor text");
+				JOptionPane.showMessageDialog(this,"Please enter a file or plain text message (Note: plain text messages must start with \"**\")");
 			}
+			cipherChoices = new char[]{(char) emptySpace.getSelectedItem(), (char) aEquals.getSelectedItem(), (char)zeroEquals.getSelectedItem()};
+			/* System.out.print(cipherChoices[0]); */
+			allFieldsFilled = true;
+			for(int i = 0; i < cipherChoices.length; i++)
+			{
+				if(cipherChoices[i] == '-')
+					allFieldsFilled = false;
+			}
+			if(allFieldsFilled) // if a selection has been made for each of the ciphers, tell the main workhorse we're ready to proceed
+			{
+				//System.out.println("Ready!");
+				ready = true;
+			}
+			else // if a field isn't filled, throw an error
+			{
+				JOptionPane.showMessageDialog(this,"Please fill out all drop-down boxes");
+				return;
+			}
+		
 		}
 		//If the decrypt button is pressed
 		if(actionName.equals("Decrypt"))
@@ -135,25 +174,31 @@ public class GUI extends JFrame implements WindowListener,ActionListener{
 			 *  3. Profit 
 			 */
 			//If the user manually entered the file, we don't want them getting avoided because they didn't use the file selection button!
-			encryptFile = new File(outField.getText());
+			decryptFile = new File(outField.getText());
 			String inputText = outField.getText();
+			isEncryption = false;
 			//if the input entered was plaintext (denoted by ** at the beginning)
 			if(inputText.startsWith("**"))
 			{
-				System.out.println("Text");
+				//System.out.println("Text");
+				isFile = false;
 			}
-			else if(encryptFile.exists()) //check file exists, if not throw an error
+			else if(decryptFile.exists()) //check file exists, if not throw an error
 			{
-				System.out.println("File");
+				//System.out.println("File");
+				isFile = true;
 			}
 			else
 			{
 				System.out.println("Not file nor text");
-				new JOptionPane("Please enter a file or message");
+				JOptionPane.showMessageDialog(this,"Please enter a file or plain text message (Note: plain text messages must start with \"**\")");
+				return;
 			}
+			ready = true;
+			
 		}
 	}
-
+	
 	@Override
 	public void windowOpened(WindowEvent e) {
 		// TODO Auto-generated method stub
--- EncryptionGUI/src/Main.java
+++ EncryptionGUI/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.TimeUnit;
+
+
+
 /* EncryptionGUI.java main - This is the main driver of the code, 
  * 
  */
@@ -8,5 +12,17 @@ public class Main {
 	{
 	System.out.println("A");
 	new GUI();
+	while(GUI.isReady() == false) // while the Generate button has not been pressed
+	{
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Sleep function interrupted");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	System.out.println("Code executing!");
 	}
 }
